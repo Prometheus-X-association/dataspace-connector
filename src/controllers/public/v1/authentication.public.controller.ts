b87e37e51767452a1df348ba026e54803b246314
Visions-Nicolas <nicolas@visionspol.eu>
2024-01-08 11:23:18 +0100

    feat: authentication

---
+++ src/controllers/public/v1/authentication.public.controller.ts
@@ -0,0 +1,46 @@
+import { Request, Response, NextFunction } from "express";
+import {Configuration} from "../../../utils/types/configuration";
+import {errorRes, successRes} from "../../../libs/api/APIResponse";
+import {generateBearerTokenForPrivateRoutes} from "../../../libs/jwt";
+
+/**
+ * A template method just to show the convention used
+ */
+export const login = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+
+        // TODO LOGIN VT ?
+        const config = await Configuration.findOne({
+            serviceKey: req.body.serviceKey,
+            secretKey: req.body.secretKey
+        }).lean();
+
+        if (!config)
+            return errorRes({
+                req,
+                res,
+                code: 404,
+                errorMsg: 'Wrong credentials',
+                message:
+                    'Wrong credentials',
+            });
+
+        const token = await generateBearerTokenForPrivateRoutes(req.body.serviceKey, req.body.secretKey)
+
+        return successRes({
+            code: 200,
+            req,
+            res,
+            message: 'Successful login',
+            data: {
+                token
+            },
+        });
+    } catch (err) {
+        next(err);
+    }
+};
---
+++ src/routes/public/v1/auth.public.router.ts
@@ -0,0 +1,45 @@
+import { Router } from "express";
+import {
+    getSelfDescription
+} from "../../../controllers/public/v1/description.public.controller";
+import {login} from "../../../controllers/public/v1/authentication.public.controller";
+import {body} from "express-validator";
+const r: Router = Router();
+
+/**
+ * @swagger
+ * tags:
+ *   name: Authentication
+ *   description: Authentication to access private routes
+ */
+
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Login to use private routes
+ *     tags: [Authentication]
+ *     produces:
+ *       - application/json
+ *     requestBody:
+ *      content:
+ *       application/json:
+ *         schema:
+ *           type: object
+ *           properties:
+ *             secretKey:
+ *               description: PTX secret Key
+ *               type: string
+ *             serviceKey:
+ *               description: PTX service Key
+ *               type: string
+ *     responses:
+ *       '200':
+ *         description: Successful response
+ */
+r.post("/login", [
+    body('secretKey').isString(),
+    body('serviceKey').isString(),
+],  login);
+
+export default r;
