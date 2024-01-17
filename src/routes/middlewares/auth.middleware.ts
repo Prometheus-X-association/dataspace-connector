73d4c65ba54c0e915d3b0384c9a09e249bf78428
Visions-Nicolas <nicolas@visionspol.eu>
2024-01-17 21:41:18 +0100

    feat(auth): Bearer token

--- src/routes/middlewares/auth.middleware.ts
+++ src/routes/middlewares/auth.middleware.ts
@@ -5,9 +5,16 @@ import { verifyToken } from '../../libs/jwt';
  * Checks the validation pipeline of express-validator
  */
 export const auth = async (req: Request, res: Response, next: NextFunction) => {
-    if (!req.header('Authorization'))
-        return res.status(401).json('You need to be authenticated');
-    const jwt = await verifyToken(req.header('Authorization'));
+    if (
+        !req.header('Authorization') ||
+        !req.header('Authorization').startsWith('Bearer ')
+    ) {
+        return res
+            .status(401)
+            .json({ message: 'You need to be authenticated' });
+    }
+    const token = req.header('Authorization').slice(7);
+    const jwt = await verifyToken(token);
     if (!jwt) return res.status(401).json('You need to be Authenticated');
     else next();
 };
