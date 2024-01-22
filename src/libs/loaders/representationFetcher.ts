edb4d31a4ea2652b4a5fc1a72d762ac64b99c5f3
Visions-Nicolas <nicolas@visionspol.eu>
2024-01-22 11:19:06 +0100

    feat(services): added representationFetcher.ts for method en credential use

---
+++ src/libs/loaders/representationFetcher.ts
@@ -0,0 +1,68 @@
+import axios from 'axios';
+import { Credential } from '../../utils/types/credential';
+
+export const postRepresentation = async (
+    method: string,
+    endpoint: string,
+    data: any,
+    credential: string
+) => {
+    let cred;
+
+    if (credential && method !== 'none') {
+        cred = await getCredential(credential);
+    }
+
+    switch (method) {
+        case 'none':
+            await axios.post(endpoint, data);
+            break;
+        case 'basic':
+            await axios.post(endpoint, {
+                ...data,
+                username: cred.key,
+                password: cred.value,
+            });
+            break;
+        case 'api-header':
+            await axios.post(endpoint, data, {
+                headers: {
+                    [cred.key]: cred.value,
+                },
+            });
+            break;
+    }
+};
+
+export const getRepresentation = async (
+    method: string,
+    endpoint: string,
+    credential: string
+) => {
+    let cred;
+
+    if (credential && method !== 'none') {
+        cred = await getCredential(credential);
+    }
+
+    switch (method) {
+        case 'none':
+            return await axios.get(endpoint);
+        case 'basic':
+            // await axios.get(endpoint, {
+            //     username: cred.key,
+            //     password: cred.value,
+            // });
+            break;
+        case 'api-header':
+            return await axios.get(endpoint, {
+                headers: {
+                    [cred.key]: cred.value,
+                },
+            });
+    }
+};
+
+const getCredential = async (credential: string) => {
+    return Credential.findById(credential).lean();
+};
