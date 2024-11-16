28a3b80293b3d2136afebd5493c05dbd89108c7a
Visions-Nicolas <nicolas@visionspol.eu>
2024-11-16 02:17:51 +0100

    feat: third-party

---
+++ src/libs/third-party/postDataRequest.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { ConsentImportPayload } from '../../utils/types/consentImportPayload';
+
+/**
+ * POSTs a data request using the signed consent containing the access token
+ * @param payload The payload received from VisionsTrust to your consent/import endpoint
+ * @returns The response promise made to the Export Service
+ */
+export const postDataRequest = async (payload: ConsentImportPayload) => {
+    return await axios({
+        method: 'POST',
+        url: payload.dataProviderEndpoint,
+        data: {
+            ...payload,
+        },
+        headers: {
+            'content-type': 'application/json',
+        },
+    });
+};
