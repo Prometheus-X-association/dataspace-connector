54c4007223d3f3ad8d94c3805465f31ebd848186
Visions-Nicolas <nicolas@visionspol.eu>
2025-04-23 09:49:41 +0200

    delete template.public.router.ts

--- src/routes/public/v1/template.public.router.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { publicTemplateMethod } from '../../../controllers/public/v1/template.public.controller';
-const r: Router = Router();
-
-r.get('/', publicTemplateMethod);
-
-export default r;
