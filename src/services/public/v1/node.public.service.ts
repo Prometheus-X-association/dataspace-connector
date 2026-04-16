053de3d277e8517fd143936f4ed64fc05b35c00a
Visions-Nicolas <nicolas@visionspol.eu>
2026-04-16 18:09:07 +0200

    fix: paramsMapper fix for last node of chain

--- src/services/public/v1/node.public.service.ts
+++ src/services/public/v1/node.public.service.ts
@@ -194,9 +194,16 @@ export const nodeCallbackService = async (props: {
             if (offer.softwareResources && offer.softwareResources.length > 0) {
                 for (const softwareResource of offer.softwareResources) {
                     //look in data exchange if params exists for this resource in serviceChainParams array
-                    const resource = dataExchange.serviceChainParams.filter(
+                    let resource = dataExchange.serviceChainParams.filter(
                         (element) => element?.resource === softwareResource
                     );
+
+                    // if no nextTargetId it means that we are in the last node of the chain and we need to look for the resource in the purposes array
+                    if(!nextTargetId && resource.length === 0){
+                       resource = dataExchange.purposes.filter(
+                            (element) => element?.resource === softwareResource
+                        );
+                    }
 
                     //retrieve targetId = offer
                     const [softwareResourceSD] = await handle(
